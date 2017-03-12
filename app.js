1b52b67424c86ce177d8ee8150a3d7798a07d458
のびすけ <rsksound@gmail.com>
2017-03-12 15:48:33 +0900

    テクスチャとアニメーション

--- app.js
+++ app.js
@@ -19,42 +19,60 @@ const camera = new THREE.PerspectiveCamera( 75, canvasWidth / canvasHeight, 1, 1
 camera.position.z = 100;
 
 const geometry = new THREE.BoxGeometry( 10, 10, 10 );
-const material = new THREE.MeshBasicMaterial({
-  color     :0x00ff00,
-  wireframe :true,
-});
+// const material = new THREE.MeshBasicMaterial({
+//   color     :0x00ff00,
+//   wireframe :true,
+// });
 
+const URL = 'http://localhost:8000/n0bisuke.jpg'
 const meshs = [];
-for (let i = 0, x=0, y=0, z=0; i < 125; i++) {
-    meshs[i] = new THREE.Mesh(geometry, material);
-    x += 50;
 
-    if(i % 5 === 0){
-        y -= 50;
-        x = 0;
-    }
-
-    if(i % 25 === 0){
-        y = 0;
-        x = 0;
-        z += 50;
-    }
-    
-    meshs[i].position.x = x;
-    meshs[i].position.y = y;
-    meshs[i].position.z = z;
-    scene.add(meshs[i]);
+//テクスチャ読み込み
+const materialLoad = (cb) => {
+    const loader = new THREE.TextureLoader();
+    loader.load(URL, (texture) => {
+        const material = new THREE.MeshBasicMaterial({map: texture});
+        cb(material);
+    });
 }
-// console.log(mesh);
 
-function animate(){
+//複数生成
+const particle = (material) => {
+    for (let i = 0, x=0, y=0, z=0; i < 125; i++) {
+        meshs[i] = new THREE.Mesh(geometry, material);
+        x += 50;
+
+        if(i % 5 === 0){
+            y -= 50;
+            x = 0;
+        }
+
+        if(i % 25 === 0){
+            y = 0;
+            x = 0;
+            z += 50;
+        }
+
+        meshs[i].position.x = x;
+        meshs[i].position.y = y;
+        meshs[i].position.z = z;
+        scene.add(meshs[i]);
+    }
+}
+
+//アニメーション
+const animate = () => {
   for(let mesh of meshs){
     mesh.rotation.y += 0.01;      
   }
   render();
   window.requestAnimationFrame(animate);
 }
-animate();
+
+materialLoad((material) => {
+    particle(material);
+    animate();
+});
 
 //まだまだでません
 
